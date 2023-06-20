e66e97939224da7fc717640c7a4dfa10013813a2
Lianhao Zheng <128833289+LianhaoZ@users.noreply.github.com>
2023-06-20 17:20:23 -0500

    Update Deepcell.py

--- Deepcell.py
+++ Deepcell.py
@@ -1,12 +1,20 @@
 import os
 import numpy as np
 from skimage import io
-from deepcell_tracking import CellTracker
-from deepcell.model_zoo.tracking import GNNTrackingModel
-tm = GNNTrackingModel()
+# from deepcell_tracking import CellTracker
+# from deepcell.model_zoo.tracking import GNNTrackingModel
+# tm = GNNTrackingModel()
+from deepcell.applications.cell_tracking import CellTracking
 
-import numpy as np
 from tifffile import imread
+
+OUTPUT_DIR = '/Users/lian/Desktop/Tracking/Deepcell/Trial_run'
+
+# (raw image)
+folder_path = '/Users/lian/Desktop/Tracking/Trackmate7/11/Raw-images.tif'
+
+# (segmented image)
+folder_path1 = '/Users/lian/Desktop/Tracking/Trackmate7/11/segmented-imgs.tif'
 
 def tif_to_nparray(filepath):
     # Read the TIF file
@@ -16,80 +24,118 @@ def tif_to_nparray(filepath):
     np_array = np.array(tif_data)
 
     return np_array
-
-folder_path = '/Users/lian/Desktop/Tracking/Deepcell/Trial_run/Raw(5)Images.tif'
  
 # raw image array
 y = tif_to_nparray(folder_path) 
 y1 = y[..., np.newaxis]  
 
-folder_path1 = '/Users/lian/Desktop/Tracking/Deepcell/Trial_run/Segmented(5).tif'
- 
+print("raw image converted", y1.shape)  
 
 # segmented image array 
 X = tif_to_nparray(folder_path1)
 X1 = X[..., np.newaxis]  
 
-# X and y are the time-sequence data and their corresponding segmentations (labels), respectively.
-# model is a deepcell-tf tracking model. 
-tracker = CellTracker(movie=y1, annotation=X1, neighborhood_encoder=tm.neighborhood_encoder, tracking_model=tm.inference_model)
+print("segmented image converted", X1.shape)
+Tracking = CellTracking(model=None, neighborhood_encoder=None, distance_threshold=64, appearance_dim=32, birth=0.99, death=0.99, division=0.01, track_length=8, embedding_axis=0, crop_mode='resize', norm=True)
+Dict = Tracking.predict(image = y1, labels = X1)
 
-tracker.track_cells()  # runs in place, builds tracks
+# print(Dict.keys()) 
  
-# # /////// View results #
+# # # # /////// View results  in HTML /////// # # # 
 
 # # View tracked results of each batch as a video
 # # NB: This does not render well on GitHub
 # from IPython.display import HTML
 # from deepcell.utils.plot_utils import get_js_video
-
-# # Raw
-# HTML(get_js_video(np.expand_dims(y1, axis=0),
-#                   batch=0, cmap='gray'))
+# from IPython.display import display
 
 # # Tracked
 
 # # Scale the colors to match the max cell label
-# HTML(get_js_video(np.expand_dims(tracker.y_tracked, axis=0),
+# html = get_js_video(np.expand_dims(Dict['y'], axis=0),
 #                   batch=0, cmap='cubehelix', vmin=0,
-#                   vmax=len(tracker.tracks)))
+#                   vmax=len(Dict['tracks']))
+# html_filename = 'output1.html'
+# with open(html_filename, 'w') as file:
+#     file.write(html)
 
-# # ///////
 
-# # Save all tracked data and lineage files to a .trk file
-tracker.dump('./results.trk')
+# import imageio
 
-# # Open the track file
-# from deepcell_tracking.utils import load_trks
+# def convert_dict_to_tifs(data_dict, output_directory):
+#     # Set the output file name
+#     output_file = 'movie.tiff'
 
-# data = load_trks('./results.trk')
+#     # Create an empty list to store the image frames
+#     frames = []
 
-# lineage = data['lineages']  # linage information
-# X = data['X']  # raw X data
-# y = data['y']  # tracked y data
+#     # Iterate over the frames in the dictionary
+#     for frame_index, frame_data in data_dict.items():
+#         width = frame_data['width']
+#         height = frame_data['height']
+#         pixels = frame_data['pixels']
 
-import imageio
-from matplotlib import pyplot as plt
-OUTPUT_DIR = '/Users/lian/Desktop/Tracking/Deepcell/Trial_run'
-vmax = len(tracker.y_tracked)
+#         # Create an image object using Pillow
+#         image = Image.fromarray(np.array(pixels, dtype=np.uint8))
 
-raw = []
-tracked = []
+#         # Append the image to the frames list
+#         frames.append(image)
 
-for i in range(tracker.X.shape[0]):
-    new_image = tracker.X[i, ..., 0]
-    raw_path = os.path.join(OUTPUT_DIR, 'image_%d.tiff' % i)
-    imageio.imwrite(raw_path, new_image.astype('uint8'))
-    raw.append(new_image.astype('uint8'))
+#     # Save the frames as a TIFF movie using imageio
+#     imageio.mimwrite(output_file, frames, format='TIFF', fps=10)
 
-    label_image = tracker.y_tracked[i, ..., 0].astype('uint8')
-    label_path = os.path.join(OUTPUT_DIR, 'label_%d.tiff' % i)
-    plt.imsave(label_path, label_image,
-               cmap='cubehelix', vmin=0, vmax=vmax)
-    # imageio.imwrite(label_path, label_image,
-    #                 cmap='cubehelix', vmin=0, vmax=vmax)
-    tracked.append(label_image.astype('uint8'))
+ 
+# convert_dict_to_tifs(Dict, output_directory)
 
-# Make gifs
-imageio.mimsave(os.path.join(OUTPUT_DIR, 'raw.gif'), raw)
-imageio.mimsave(os.path.join(OUTPUT_DIR, 'tracked.gif'), tracked)
+# print("Done", Dict['y'].shape)
+
+# # # # /////// Access as gif /////// # # # 
+
+# from matplotlib import pyplot as plt
+# import imageio
+
+# vmax = len(Dict["y"])
+
+# # raw = []
+# tracked = []
+
+# for i in range(Dict["X"].shape[0]):
+#     # new_image = Dict["X"][i, ..., 0]
+#     # raw_path = os.path.join(OUTPUT_DIR, 'image_%d.tiff' % i)
+#     # imageio.imwrite(raw_path, new_image.astype('uint8'))
+#     # raw.append(new_image.astype('uint8'))
+
+#     label_image = Dict["y"][i, ..., 0].astype('uint8')
+#     label_path = os.path.join(OUTPUT_DIR, 'label_%d.tiff' % i)
+#     plt.imsave(label_path, label_image,
+#                cmap='cubehelix', vmin=0, vmax=vmax)
+#     # imageio.imwrite(label_path, label_image,
+#     #                 cmap='cubehelix', vmin=0, vmax=vmax)
+#     tracked.append(label_image.astype('uint8'))
+
+# # Make gifs
+# # imageio.mimsave(os.path.join(OUTPUT_DIR, 'raw.gif'), raw)
+# imageio.mimsave(os.path.join(OUTPUT_DIR, 'tracked.gif'), tracked)
+
+# # # # /////// Access as CSV /////// # # # 
+
+import csv
+  
+# Specify the output CSV file path
+output_file = 'data.csv'
+
+# Extract keys and values from the dictionary
+# keys = list(Dict.keys()[1:])
+# values = list(Dict.values()[1:])
+keys = Dict["tracks"].values()
+values = Dict["tracks"].values()
+# print(tracks)
+# for i in keys:
+#     print(type(Dict[i]))
+  
+
+# Write the dictionary data to a CSV file
+with open(output_file, 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(keys)
+    writer.writerow(values)
