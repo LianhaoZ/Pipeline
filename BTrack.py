bd70015df676535d7a03f933b9af9c67e20ddb55
Lianhao Zheng <128833289+LianhaoZ@users.noreply.github.com>
2023-07-06 17:00:57 -0500

    Update BTrack.py

--- BTrack.py
+++ BTrack.py
@@ -1,39 +1,27 @@
-import os
-import json
-
 import btrack
-import napari
-
-import numpy as np
-import pandas as pd
-
-from skimage import io
-from napari.utils import nbscreenshot
-
+from skimage.io import imread
 from btrack import datasets
+from btrack.io import export_CSV
+# load your segmentation data
+segmentation = imread('/Users/lian/Desktop/Tracking/BTrack/segmented.tif')
 
-# example segmentation
-segmentation = datasets.example_segmentation()
-
-# example config
-config = datasets.cell_config()
-
-# example objects
-objects = datasets.example_track_objects()
-
-# objects = btrack.dataio.import_CSV('napari_example.csv')
-
+# create btrack objects (with properties) from the segmentation data
+# (you can also calculate properties, based on scikit-image regionprops)
+objects = btrack.utils.segmentation_to_objects(
+  segmentation, properties=('area', )
+)
+CONFIG_FILE = datasets.cell_config()
 # initialise a tracker session using a context manager
 with btrack.BayesianTracker() as tracker:
 
   # configure the tracker using a config file
-  tracker.configure_from_file('./config.json')
+  tracker.configure(CONFIG_FILE)
 
   # append the objects to be tracked
   tracker.append(objects)
 
-  # set the volume (Z axis volume is set very large for 2D data)
-  tracker.volume=((0,1600), (0,1200), (-1e5,1e5))
+  # set the volume (Z axis volume limits default to [-1e5, 1e5] for 2D data)
+  tracker.volume = ((0, 1200), (0, 1600))
 
   # track them (in interactive mode)
   tracker.track_interactive(step_size=100)
@@ -41,15 +29,34 @@ with btrack.BayesianTracker() as tracker:
   # generate hypotheses and run the global optimizer
   tracker.optimize()
 
-  # get the tracks in a format for napari visualization
-  data, properties, graph = tracker.to_napari(ndim=2)
+#   # store the data in an HDF5 file
+#   tracker.export('/path/to/tracks.h5', obj_type='obj_type_1')
 
-vertices = data[:, 1:]
+  # get the tracks as a python list
+  tracks = tracker.tracks
 
-stack = io.imread('/Users/lian/Desktop/Tracking/Trackmate7/11/segmented-imgs.tif')
+  # optional: get the data in a format for napari
+  data, properties, graph = tracker.to_napari()
 
-with napari.gui_qt():
-    viewer = napari.Viewer()
-    viewer.add_image(stack)
-    viewer.add_points(vertices, size=4, name='points', opacity=0.3)
-    viewer.add_tracks(data, properties=properties, graph=graph, name='tracks')
+export_CSV("/Users/lian/Desktop/Tracking/BTrack/output.csv",tracks)
+# get the first track
+track_zero = tracks[0]
+
+# print the length of the track
+print(len(track_zero))
+
+# print all of the xyzt positions in the track
+print(track_zero.x)
+print(track_zero.y)
+print(track_zero.z)
+print(track_zero.t)
+
+# print the fate of the track
+print(track_zero.fate)
+
+# print the track ID, root node, parent node, children and generational depth
+print(track_zero.ID)
+print(track_zero.root)
+print(track_zero.parent)
+print(track_zero.children)
+print(track_zero.generation)
